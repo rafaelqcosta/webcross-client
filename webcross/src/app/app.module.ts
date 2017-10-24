5a53604025a8344d632e095a2bf741e48ae5d31d
rafaelqcosta <rafaqcost@gmail.com>
2017-10-24 20:09:42 -0200

    add bootstrap

--- webcross/src/app/app.module.ts
+++ webcross/src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -8,7 +11,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
